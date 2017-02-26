46174b8da83d27dabf8dede9d0e0f70af35a4e1f
Otavio Santana <otaviojava@java.net>
2017-02-26 15:21:35 -0300

    Updates with new current api

--- src/main/java/org/jnosql/book/demo/diana/chaper2/MoneyExampleApp.java
+++ src/main/java/org/jnosql/book/demo/diana/chaper2/MoneyExampleApp.java
@@ -3,6 +3,8 @@ package org.jnosql.book.demo.diana.chaper2;
 
 import java.util.List;
 import java.util.Set;
+
+import org.jnosql.diana.api.TypeReference;
 import org.jnosql.diana.api.Value;
 
 public class MoneyExampleApp {
@@ -10,7 +12,9 @@ public class MoneyExampleApp {
     public static void main(String[] args) {
         Value value = Value.of("BRL 10.0");
         Money money = value.get(Money.class);
-        List<Money> list = value.getList(Money.class);
-        Set<Money> set = value.getSet(Money.class);
+        List<Money> list = value.get(new TypeReference<List<Money>>() {
+        });
+        Set<Money> set = value.get(new TypeReference<Set<Money>>() {
+        });;
     }
 }
--- src/main/java/org/jnosql/book/demo/diana/chaper2/ValueExample.java
+++ src/main/java/org/jnosql/book/demo/diana/chaper2/ValueExample.java
@@ -1,10 +1,12 @@
 package org.jnosql.book.demo.diana.chaper2;
 
 
+import org.jnosql.diana.api.TypeReference;
+import org.jnosql.diana.api.Value;
+
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
-import org.jnosql.diana.api.Value;
 
 public class ValueExample {
 
@@ -12,9 +14,12 @@ public class ValueExample {
     public static void main(String[] args) {
         Value value = Value.of(12);
         String string = value.get(String.class);
-        List<Integer> list = value.getList(Integer.class);
-        Set<Long> set = value.getSet(Long.class);
-        Stream<Integer> stream = value.getStream(Integer.class);
-        Integer integer = value.cast();
+        List<Integer> list = value.get(new TypeReference<List<Integer>>() {
+        });
+        Set<Long> set = value.get(new TypeReference<Set<Long>>() {
+        });
+        Stream<Integer> stream = value.get(new TypeReference<Stream<Integer>>() {
+        });
+        Object integer = value.get();
     }
 }
