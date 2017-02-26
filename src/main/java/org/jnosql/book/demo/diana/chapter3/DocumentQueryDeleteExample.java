b3fcf45d4a7545ca26c7139f56e628e256ae9a24
Otavio Santana <otaviojava@java.net>
2017-02-26 18:20:03 -0300

    adds document delete query

---
+++ src/main/java/org/jnosql/book/demo/diana/chapter3/DocumentQueryDeleteExample.java
@@ -0,0 +1,35 @@
+package org.jnosql.book.demo.diana.chapter3;
+
+
+import org.jnosql.diana.api.Sort;
+import org.jnosql.diana.api.document.Document;
+import org.jnosql.diana.api.document.DocumentCollectionManager;
+import org.jnosql.diana.api.document.DocumentCollectionManagerAsync;
+import org.jnosql.diana.api.document.DocumentCondition;
+import org.jnosql.diana.api.document.DocumentDeleteQuery;
+import org.jnosql.diana.api.document.DocumentEntity;
+import org.jnosql.diana.api.document.DocumentQuery;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public class DocumentQueryDeleteExample {
+
+    public static void main(String[] args) {
+
+
+        DocumentCollectionManager manager = null;
+        DocumentCollectionManagerAsync managerAsync = null;
+
+        DocumentDeleteQuery query = DocumentDeleteQuery.of("collection");
+        DocumentCondition ageBiggerTen = DocumentCondition.gt(Document.of("age", 10));
+        query.and(ageBiggerTen);
+
+
+        manager.delete(query);
+
+        managerAsync.delete(query);
+        managerAsync.delete(query, v -> {});
+    }
+}
