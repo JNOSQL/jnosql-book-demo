08f6cd6355c980f7b2722cdfe18a93163acfb630
Otavio Santana <otaviojava@java.net>
2017-02-28 07:11:23 -0300

    Adds cdi

--- src/main/java/org/jnosql/book/demo/chapter4/DocumentRepositorySample.java
+++ src/main/java/org/jnosql/book/demo/chapter4/DocumentRepositorySample.java
@@ -2,20 +2,25 @@ package org.jnosql.book.demo.chapter4;
 
 
 import org.jnosql.artemis.document.DocumentRepository;
+import org.jnosql.artemis.document.DocumentRepositoryAsync;
 import org.jnosql.diana.api.document.Document;
 import org.jnosql.diana.api.document.DocumentCondition;
 import org.jnosql.diana.api.document.DocumentDeleteQuery;
 import org.jnosql.diana.api.document.DocumentQuery;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public class DocumentRepositorySample {
 
     public static void main(String[] args) {
         DocumentRepository repository = null;
+
+
 
         Person person = new Person();
         person.setAddress("Olympus");
@@ -27,6 +32,7 @@ public class DocumentRepositorySample {
 
         Person personUpdated = repository.save(person);
         repository.save(people);
+        repository.save(person, Duration.ofHours(1L));
 
         repository.update(person);
         repository.update(people);
@@ -40,5 +46,19 @@ public class DocumentRepositorySample {
 
         DocumentDeleteQuery deleteQuery = query.toDeleteQuery();
         repository.delete(deleteQuery);
+
+
+        DocumentRepositoryAsync repositoryAsync = null;
+        Consumer<Person> callback = p -> {};
+        repositoryAsync.save(person);
+        repositoryAsync.save(person, Duration.ofHours(1L));
+        repositoryAsync.save(person, callback);
+        repositoryAsync.save(people);
+
+        repositoryAsync.update(person);
+        repositoryAsync.update(person, callback);
+        repositoryAsync.update(people);
+
     }
+
 }
