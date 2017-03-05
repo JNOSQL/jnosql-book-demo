26871dcfba0252f51c71191334d0a3eb6e4df2cf
Otavio Santana <otaviojava@java.net>
2017-03-05 09:57:54 -0300

    add more smaple

--- src/main/java/org/jnosql/book/demo/chaper2/KeyValueEntityApp.java
+++ src/main/java/org/jnosql/book/demo/chaper2/KeyValueEntityApp.java
@@ -9,5 +9,9 @@ public class KeyValueEntityApp {
     public static void main(String[] args) {
         KeyValueEntity<String> entity = KeyValueEntity.of("key", Value.of(123));
         KeyValueEntity<Integer> entity2 = KeyValueEntity.of(12, "Text");
+        String key = entity.getKey();
+        Value value = entity.getValue();
+        Integer integer = entity.get(Integer.class);
+
     }
 }
